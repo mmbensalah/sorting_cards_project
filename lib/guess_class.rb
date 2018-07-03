97acd4b453a25258af85bc75e8a3ac649be087da
mmbensalah <mmbensalah@gmail.com>
2018-07-02 18:01:03 -0600

    add guess_class.rb

---
+++ lib/guess_class.rb
@@ -0,0 +1,23 @@
+class Guess
+  attr_reader :response, :card
+  def initialize(response, card)
+    @response = response
+    @card = card
+  end
+
+  def correct?
+    if "#{card.value} of #{card.suit}" == @response
+      true
+    else
+      false
+    end
+  end
+
+  def feedback
+    if "#{card.value} of #{card.suit}" == @response
+      "Correct!"
+    else
+      "Incorrect!"
+    end
+  end
+end
