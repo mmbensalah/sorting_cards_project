5170f4aa14f09dfe0abf30c6ecc9c90202396221
mmbensalah <mmbensalah@gmail.com>
2018-07-02 19:41:16 -0600

    added round_class files

---
+++ lib/round_class.rb
@@ -0,0 +1,5 @@
+require "./lib/sorting_cards"
+require "./lib/deck_class"
+
+class Round
+  def initialize(cards)
---
+++ test/round_class_test.rb
@@ -0,0 +1,10 @@
+require "pry"
+require "./lib/sorting_cards"
+require "./lib/deck_class"
+require "./lib/round_class"
+require "minitest/autorun"
+require "minitest/pride"
+
+class RoundTest < Minitest::Test
+
+  def test_it_exists
