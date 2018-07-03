bebeeb48edd627999bff5934f0ec7fe0b10c5a20
mmbensalah <mmbensalah@gmail.com>
2018-07-03 06:28:26 -0600

    added def deck and def test_deck

--- lib/round_class.rb
+++ lib/round_class.rb
@@ -1,5 +1,22 @@
 require "./lib/sorting_cards"
 require "./lib/deck_class"
+require "./lib/guess_class"
+
+# attr_reader :deck
 
 class Round
-  def initialize(cards)
+  def initialize(deck)
+  @deck = deck
+  end
+
+  def deck
+    Deck.new
+  end
+
+  def guesses(response)
+    if @response < 0
+      0
+    end
+  end
+
+end
