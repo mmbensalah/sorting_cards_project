fa8e6e0596c1056f92e721c771828857988a5cb1
mmbensalah <mmbensalah@gmail.com>
2018-07-04 13:39:58 -0600

    minor changes to sort and sort_test

---
+++ lib/sort.rb
@@ -0,0 +1,23 @@
+require "./lib/round_class"
+require 'pry'
+
+value:{
+  one: 1
+  two: 2
+  three: 3
+  four: 4
+  five: 5
+  six: 6
+  seven: 7
+  eight: 8
+  nine: 9
+  ten: 10
+  Jack: 11
+  Queen: 12
+  King: 13
+  Ace: 14
+  Clubs: .25
+  Diamonds: .5
+  Hearts: .75
+  Spades: 1.0
+}
